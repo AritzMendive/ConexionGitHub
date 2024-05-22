d4392c1965524b87233c53e0c782d6504bdaa726
EiderLabiano <149406571+EiderLabiano@users.noreply.github.com>
2024-05-22 19:16:18 +0200

    Cambios jiji

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main{
     public static void main(String [] Args){
         System.out.println("Hola github! Mi usuario es AritzMendive");
         System.out.println("Hola Aritz! Soy Cembo el Crack");
+        System.out.println("Hola Aritz! Soy Eider UwU");
     }
 }
